5ddeedc06d25c40c2cd008383be5d20050f0be8f
Alexander Erlandsson <alexander@ourstudio.se>
2020-07-03 15:49:26 +0200

    Added aria attributes to progress bar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -17,7 +17,12 @@ const ProgressBar = ({ unit, valueCurrent, valueMax }: Props) => {
         Visar {valueCurrent.toString()} av {valueMax.toString()} {unit}
       </Description>
       <Bar>
-        <BarActive style={{ width: `${barWidth}%` }} />
+        <BarActive
+          style={{ width: `${barWidth}%` }}
+          aria-valuenow={valueCurrent}
+          aria-valuemin={0}
+          aria-valuemax={valueMax}
+        />
       </Bar>
     </Wrapper>
   );
