ab05d41e7fd2f080b0ae5c79d7295f3264e1fb5a
Jonatan Josefsson <jonatan@ourstudio.se>
2021-03-19 13:28:10 +0100

    quick nav video player for thumnail

---
+++ src/components/Video/QuickNavVideo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CustomPlayer from './CustomPlayer';
+
+interface QuickNavVideo {
+    src?: string;
+    index: number;
+}
+
+const QuickNavVideo = ({ src, index }: QuickNavVideo) => {
+    return (
+        <CustomPlayer key={index} url={src} ratio="66.666%" />
+    )
+};
+
+export default QuickNavVideo;
