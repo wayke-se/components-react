e2457afbf2da0b1cba9cae0e8f7fca52b367e0f5
Jonatan Josefsson <jonatan@ourstudio.se>
2021-04-20 08:14:07 +0200

    added preload metadata

--- src/components/Video/CustomPlayer.tsx
+++ src/components/Video/CustomPlayer.tsx
@@ -1,6 +1,5 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Wrapper } from './wrapper';
-import useHasBeenVisible from '../../hooks/useHasBeenVisible';
 import Video from './Video';
 
 type PropsType = {
@@ -12,17 +11,13 @@ type PropsType = {
 };
 
 const CustomPlayer = ({ url, controls, modal, ratio = '56.25%', isPreview }: PropsType) => {
-  const ref = useRef<HTMLVideoElement | null>(null);
-  const isVisible = useHasBeenVisible(ref);
-
   return (
     <Wrapper ratio={ratio} isPreview={isPreview}>
       <Video
         key={url}
-        ref={ref}
         modal={modal}
+        preload="metadata"
         controls={controls}
-        preload={isVisible ? 'metadata' : 'none'}
         width="100%"
         height="100%"
         src={url}
