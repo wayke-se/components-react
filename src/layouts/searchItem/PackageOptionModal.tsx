39de77fca25de905a6018c6e9743c7fd3412bb48
Peter <peter@ourstudio.se>
2023-11-13 10:05:43 +0100

    update package modal link title

--- src/layouts/searchItem/PackageOptionModal.tsx
+++ src/layouts/searchItem/PackageOptionModal.tsx
@@ -6,6 +6,7 @@ import { ButtonInline } from '../../components/Button';
 import Modal from '../../components/Modal';
 import Content from '../../components/Content';
 import LogoBox from '../../components/LogoBox';
+import { useTranslation } from 'react-i18next';
 
 interface ModelLink {
   href?: string | null;
@@ -24,31 +25,34 @@ interface PackageOptionsModalProps {
   onClose: () => void;
 }
 
-const PackageOptionModal = ({ packageOption, onClose }: PackageOptionsModalProps) => (
-  <Modal title={packageOption.title || ''} onClose={onClose}>
-    {packageOption.image && (
-      <Repeat>
-        <LogoBox logo={packageOption.image} alt={packageOption.title || undefined} wide />
-      </Repeat>
-    )}
-    {packageOption.description && (
-      <Repeat>
-        <Content dangerouslySetInnerHTML={{ __html: marked(packageOption.description) }} />
-      </Repeat>
-    )}
-    {packageOption.link?.href && packageOption.link.title && (
-      <Repeat>
-        <ButtonInline
-          as="a"
-          href={packageOption.link.href}
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-        >
-          {packageOption.link.title}
-        </ButtonInline>
-      </Repeat>
-    )}
-  </Modal>
-);
+const PackageOptionModal = ({ packageOption, onClose }: PackageOptionsModalProps) => {
+  const { t } = useTranslation();
+  return (
+    <Modal title={packageOption.title || ''} onClose={onClose}>
+      {packageOption.image && (
+        <Repeat>
+          <LogoBox logo={packageOption.image} alt={packageOption.title || undefined} wide />
+        </Repeat>
+      )}
+      {packageOption.description && (
+        <Repeat>
+          <Content dangerouslySetInnerHTML={{ __html: marked(packageOption.description) }} />
+        </Repeat>
+      )}
+      {packageOption.link?.href && (
+        <Repeat>
+          <ButtonInline
+            as="a"
+            href={packageOption.link.href}
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            {packageOption.link.title ? packageOption.link.title : t('item.readMoreInNewTab')}
+          </ButtonInline>
+        </Repeat>
+      )}
+    </Modal>
+  );
+};
 
 export default PackageOptionModal;
